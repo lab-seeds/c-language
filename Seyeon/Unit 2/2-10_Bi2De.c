c6e254e401f49526fb5131e3bd85155ac919edc8
seyeon <47268456+seyeon040768@users.noreply.github.com>
2019-02-23 15:36:13 +0900

    Update 2-10_Bi2De.c

--- Seyeon/Unit 2/2-10_Bi2De.c
+++ Seyeon/Unit 2/2-10_Bi2De.c
@@ -4,9 +4,18 @@ int main(void)
 {
 	int a[6] = {1, 0, 1, 0, 1, 0};
 	int num = 0;
-	for(int i = 0; i < sizeof(a) / sizeof(int); i++)
+	int temp;
+	int size = sizeof(a) / sizeof(int);
+	
+  	for (int i = 0; i < size / 2; i++) 
 	{
-		num += pow(a[i] * 2, i);
+    	temp = a[i];
+    	a[i] = a[(size - 1) - i];
+    	a[(size - 1) - i] = temp;
+    }
+	for(int i = 0; i < size; i++)
+	{
+		num += pow(2, i) * a[i];
 	}
 	printf("%d", num);
 	
