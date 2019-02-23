d1c3f2dcc880b50a888b73ad6c06f9dabe7a7e04
seyeon <47268456+seyeon040768@users.noreply.github.com>
2019-02-23 15:24:44 +0900

    Update 2-10_Bi2De.c

--- Seyeon/Unit 2/2-10_Bi2De.c
+++ Seyeon/Unit 2/2-10_Bi2De.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 #include<math.h>
-
 int main(void)
 {
 	int a[6] = {1, 0, 1, 0, 1, 0};
 	int num = 0;
-  
-	for(int i = 0; i < 6; i++)
+	for(int i = 0; i < sizeof(a) / sizeof(int); i++)
 	{
 		num += pow(a[i] * 2, i);
 	}
