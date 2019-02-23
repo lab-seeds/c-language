b151e809b15b2b440f33ea39ec7b79c6bd9af970
seyeon <47268456+seyeon040768@users.noreply.github.com>
2019-02-23 15:13:43 +0900

    Create 2-10_HEX.c

---
+++ Seyeon/Unit 2/2-10_HEX.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int num = 72;
+	int a[100] = {0};
+	int i = 0;
+	int j = i;
+	while(1)
+	{
+		a[i] = num % 2;
+		num /= 2;
+		j++;
+		if(num == 0)
+		{
+			break;
+		}
+	}
+	printf("%d ", 1);
+	for(i = j - 1; i >= 0; i--)
+	{
+		printf("%d ", a[i]);
+	}
+	
+	return 0;
+}
