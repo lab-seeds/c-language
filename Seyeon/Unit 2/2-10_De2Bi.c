4bb0191b877840ce4517a39d6776008606746ee4
seyeon <47268456+seyeon040768@users.noreply.github.com>
2019-03-17 16:19:52 +0900

    Update 2-10_De2Bi.c

--- Seyeon/Unit 2/2-10_De2Bi.c
+++ Seyeon/Unit 2/2-10_De2Bi.c
@@ -2,21 +2,17 @@
 
 int main(void)
 {
-	int num = 72;
+	int num = 45;
 	int a[100] = {0};
 	int i = 0;
 	int j = i;
-	while(1)
+	do
 	{
 		a[j] = num % 2;
 		num /= 2;
 		j++;
-		if(num == 0)
-		{
-			break;
-		}
-	}
-	printf("%d ", 1);
+	}	while(num != 0);
+	
 	for(i = j - 1; i >= 0; i--)
 	{
 		printf("%d ", a[i]);
